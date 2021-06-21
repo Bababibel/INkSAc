2d6cb94591ea4f12577cd96f9aa0b6cdf54b3308
Bardon Robin <robin.bardon@insa-cvl.fr>
2021-06-21 18:13:41 +0200

    Avancement

--- app/screens/RequestScreen.js
+++ app/screens/RequestScreen.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
-import {View, Text,TouchableOpacity, FlatList } from 'react-native';
+import {View, Text,TouchableOpacity, FlatList, Modal, Button } from 'react-native';
 import { globalStyles, globalColors } from '../assets/styles/global_styles';
 import Card from '../assets/shared/RequestCard';
 
 export default function RequestScreen({ navigation }){
+    const [modalOpen, setModalOpen] = useState(false)
+
     const [list, setList] = useState([
         {title: 'Cours Maths', author: 'Mme Gauthier', comment:'Meilleur cours de l\'année', expirationDate: '10-6', key: '1'},
         {title: 'Rapport SHS', author: 'Mme remerciment', comment:'Pas mal votre rapport', expirationDate: '30-08', key: '2'},
@@ -19,11 +21,19 @@ export default function RequestScreen({ navigation }){
     
     return (
         <View style={globalStyles.container}>
+            <Modal visible={modalOpen}>
+                <View>
+                    <Button onPress={() => setModalOpen(false)} title='close'/>
 
+                </View>
+            </Modal>
             <FlatList
                 data={list}
                 renderItem={({item}) => (
-                    <TouchableOpacity>
+                    <TouchableOpacity onPress={() => {
+                        setModalOpen(true)
+                        
+                    }}>
                         <Card>
                             <Text style={globalStyles.titleText}>{ item.title }</Text>
                         </Card>
