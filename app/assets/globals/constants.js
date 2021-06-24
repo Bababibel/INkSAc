303c74a7cf126d8e2d01bfeb253a0660082e0c4c
Bababibel <baptiste.setechnic@gmail.com>
2021-06-24 08:40:23 +0200

    Upgraded form to post a file and request. File is uploaded, request too and both are linked. Need to implement list. Constants for user and urls first try.

--- app/assets/globals/constants.js
+++ app/assets/globals/constants.js
@@ -1,4 +1,6 @@
 export default {
     uploadUrl: 'https://bgauthier.fr/inksac/api/upload.php',
-    postFile: 'https://bgauthier.fr/inksac/api/file/createFile.php'
+    postFile: 'https://bgauthier.fr/inksac/api/file/createFile.php',
+    postRequest: 'https://bgauthier.fr/inksac/api/request/createRequest.php',
+    addFileToRequest: 'https://bgauthier.fr/inksac/api/link/addFileToRequest.php'
 }
--- app/assets/modules/UploadFormModule.js
+++ app/assets/modules/UploadFormModule.js
@@ -1,15 +1,20 @@
+/*
+##########################################################################
+#  Form module to upload files and post a request. WEB compatible ONLY ! #
+##########################################################################
+*/
+
 import axios from 'axios';
 import React,{Component} from 'react';
 import { Button, View, Text, StyleSheet } from 'react-native';
 
 import { globalStyles } from "../styles/global_styles";
 import constants from '../globals/constants';
+import { toSqlFormat } from '../../tools/dateConverter';
 
 
-const minDeadline = new Date();
-const minDelivery = new Date();
-minDeadline.setDate(minDeadline.getDate() + 7);
-minDelivery.setDate(minDeadline.getDate() + 7);
+
+
 
 
 function setFileName(value) {
@@ -22,86 +27,97 @@ class UploadForm extends Component {
 	    selectedFile: null
 	};
     
-
-
 	onFileChange = event => {
 	    this.setState({ selectedFile: event.target.files[0] });
 	};
 	
 	onFileUpload = async () => {
-        /*let response = await fetch(constants.uploadUrl, {
-            method: 'POST',
-            body: new FormData(file)
-        });
-    
-        let result = await response.json();
-    
-        alert(result.message);*/
-        const formData = new FormData();
-        formData.append("file",
+        const fileFormData = new FormData();
+        // Create the post request to grab data from $_POST['file] in the php server
+        fileFormData.append("file",
             this.state.selectedFile,
             this.state.selectedFile.name,   
         );
-        
-        axios.post(constants.uploadUrl, formData, {
+        axios.post(constants.uploadUrl, fileFormData, {
             headers: {'Content-Type': 'multipart/form-data'},
         })
         .then(response => {
-            let path = response.data.path;
-            //let newFormData = new FormData();
-            //newFormData.append('BITE', 'GROSEXE');
-            var myForm = document.getElementById('fileData');
-            let newFormData = new FormData(myForm);
-            newFormData.append('path', path);
-            newFormData.append('name', this.state.selectedFile.name);
-            for (var key of newFormData.entries()) {
-                console.log(key[0] + ', ' + key[1])
+            // Grab path from return field
+            if ('path' in response.data) {
+                let path = response.data.path;
+                let myForm = document.getElementById('fileData');
+                let createFileFormData = new FormData(myForm);
+                createFileFormData.append('path', path);
+                createFileFormData.append('name', this.state.selectedFile.name);
+                // Use API to create the File in database with the path
+                axios.post(constants.postFile, createFileFormData, {
+                    headers: {'Content-Type': 'multipart/form-data'},
+                })
+                .then(response => {
+                    if ('message' in response.data) {
+                        console.log("File creating request: " + response.data.message);
+                        this.uploadRequest(path);
+                    }
+                    else {
+                        console.log('Internal error. Please try again or contact the support team.');
+                    }
+                })
             }
-            axios.post(constants.postFile, newFormData, {
-                headers: {'Content-Type': 'multipart/form-data'},
-            })
-            .then(response => {
+            else if ('message' in response.data) {
                 console.log(response.data.message);
-            })
-
+            }
+            else {
+                console.log('Internal error. Please try again or contact the support team.');
+            }
         });
 	};
-	// File content to be displayed after
-	// file upload is complete
-	fileData = () => {
-        if (this.state.selectedFile) {
-            return (
-            <div>
-                <p>Nom original: {this.state.selectedFile.name}</p>
-                <p>Extension: {this.state.selectedFile.type}</p>
-            </div>
-            );
-        }
-        else {
-            return (
-            <div>
-                <h4>Choisissez un fichier à imprimer</h4>
-            </div>
-            );
-        }
+
+    uploadRequest = async (file_path) => {
+        let myForm = document.getElementById('requestData');
+        let createRequestFormData = new FormData(myForm);
+        let delivery_date = createRequestFormData.get('delivery_date');
+        let deadline = createRequestFormData.get('deadline');
+        /* Edit date to convert into a correct format */
+        const deadlineDate = new Date();
+        deadlineDate.setDate(deadlineDate.getDate() + 7);
+        const deadlineDateFormatted = toSqlFormat(deadlineDate);
+        const deliveryDate = new Date();
+        deliveryDate.setDate(deliveryDate.getDate() + 10);
+        const deliveryDateFormatted = toSqlFormat(deliveryDate);
+        createRequestFormData.append('delivery_date', deliveryDateFormatted);
+        createRequestFormData.append('deadline', deadlineDateFormatted);
+        createRequestFormData.append('author', 1);
+        axios.post(constants.postRequest, createRequestFormData, {
+            headers: {'Content-Type': 'multipart/form-data'},
+        })
+        .then(response => {
+            console.log(response.data)
+            if ('id' in response.data) {
+                let createLinkToFileData = new FormData();
+                createLinkToFileData.append('request_id', response.data.id);
+                createLinkToFileData.append('file_path', file_path);
+                console.log(file_path);
+                axios.post(constants.addFileToRequest, createLinkToFileData, {
+                    headers: {'Content-Type': 'multipart/form-data'},
+                })
+                .then(response => {
+                    console.log(response.data);
+                })
+            }
+            else {
+                console.log('Unable to post request and get its id.');
+            }
+        })
     };
 
-    requestData = () => {
+    fileForm = () => {
         if (this.state.selectedFile) {
             return (
                 <div>
-                    <form id="fileData">
+                    <form id="fileData" style={styles.fileForm}>
                         <label>Titre du fichier<br/>
                             <input type="text" name="fileName" value={this.state.selectedFile.name} onChange={setFileName} style={styles.input}/> <br/>
                         </label>
-                        {/*<label>Deadline souhaitée<br/>
-                            <input type="date" name="deadlineDate" min={minDeadline} style={styles.input} /><br/>
-                        </label>
-                        <label>Date d'impression souhaitée<br/>
-                            <input type="date" name="deliveryDate" min={minDelivery} style={styles.input}/> <br/>
-                        </label>*/}
-
-
                         <label>Couleur
                             <span><input type="radio" name="color" value="0" onChange={setFileName} checked style={styles.smallInput}/><label>Non</label></span>
                             <span><input type="radio" name="color" value="1" onChange={setFileName} style={styles.smallInput}/><label>Oui</label></span>
@@ -134,24 +150,53 @@ class UploadForm extends Component {
                         </label>
                     </form>
                 </div>
+            );
+        }
+    };
+
+    requestForm = () => {
+        if (this.state.selectedFile) {
+            return (
+                <div>
+                    <form id="requestData" style={styles.fileForm}>
+                        <h1>Demande une requête</h1>
+                        <label>Deadline de vote souhaitée<br/>
+                            <input type="date" name="deadline" style={styles.smallInput} /><br/>
+                        </label>
+                        <label>Date d'impression souhaitée<br/>
+                            <input type="date" name="delivery_date" style={styles.smallInput}/> <br/>
+                        </label>
+
+                        <label>Titre de la requête<br/>
+                            <input type="text" name="title" style={styles.smallInput}/> <br/>
+                        </label>
+                        <label>Commentaire (visible de tous)<br/>
+                            <input type="text" name="comment" style={styles.largeInput}/> <br/>
+                        </label>
+                        <label>Requête cachée aux élèves
+                            <span><input type="radio" name="hidden" value="0" onChange={setFileName} checked style={styles.smallInput}/><label>Non</label></span>
+                            <span><input type="radio" name="hidden" value="1" onChange={setFileName} style={styles.smallInput}/><label>Oui</label></span>
+                        </label><br/>
+                    </form>
+                </div>
                 );
             }
         };
-        render() {
-        return (
-            <View style={[globalStyles.container, styles.maxHeight]}>
+    render() {
+    return (
+        <View style={[globalStyles.container, styles.maxHeight]}>
+            <form id="file" style={styles.form}>
                 <Text style={[globalStyles.titleText, styles.title]}>
                     Formulez une nouvelle demande
                 </Text>
-                <form id="file" style={styles.form}>
-                    <input type="file" onChange={this.onFileChange} style={styles.input}/>
-                </form>
-                {this.fileData()}
-                {this.requestData()}
-                <button onClick={this.onFileUpload}>Enregistrer la requête</button>
-            </View>
-        );
-	}
+                <input type="file" onChange={this.onFileChange} style={styles.input}/>
+            </form>
+            {this.fileForm()}
+            {this.requestForm()}
+            <button onClick={this.onFileUpload}>Enregistrer la requête</button>
+        </View>
+    );
+}
 }
 
 export default UploadForm;
@@ -163,13 +208,18 @@ const styles = {
     form: {
         display: 'flex',
         flexDirection: 'column',
-        gap: 20,
+        gap: 10,
+        fontFamily: 'ubuntu-regular',
+    },
+    fileForm: {
+        display: 'flex',
+        flexDirection: 'column',
+        gap: 5,
         fontFamily: 'ubuntu-regular',
     },
     input: {
         width: '100%',
         marginTop: 10,
-        marginBottom: 20
     },
     largeInput: {
         width: '100%',
@@ -177,11 +227,7 @@ const styles = {
         marginTop: 10,
         height: 50,
     },
-    largeInput: {
-        height: 10,
-        marginBottom: 20,
-        marginTop: 10,
-        height: 50,
+    smallInput: {
     },
     title: {
         fontSize: 35,
